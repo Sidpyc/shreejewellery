9b3362488b961f0f5827bd718cd24e99f91777de
SidPyc <siddhufs2003@gmail.com>
2024-08-24 09:21:54 +0530

    removed unused components

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import React from 'react'
 import Carousel from './components/Carousel'
 import Navbar from './components/Navbar'
 import Aboutus from './components/Aboutus'
-import Jewelry from './components/Jewelry'
 import Contactus from './components/Contactus'
 import Location from './components/Location'
-import Craftmanship from './components/Craftmanship'
 import Footer from './components/Footer'
 import Carouseltwo from './components/Carouseltwo'
 import Carouselthree from './components/Carouselthree'
@@ -16,9 +14,7 @@ const page = () => {
       <Navbar />
       <Carousel/>
       <Aboutus />
-      {/* <Jewelry /> */}
       <Carouselthree/>
-      {/* <Craftmanship /> */}
       <Carouseltwo/>
       <Contactus />
       <Location />
